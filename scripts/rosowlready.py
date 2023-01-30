ec645bfd0d8aa32410d7dab9efbdf7b6febecad0
Alberto Olivares Alarcos <aolivares@iri.upc.edu>
2023-01-30 13:06:22 +0100

    added new service to query the ontology using SELECT

--- scripts/rosowlready.py
+++ scripts/rosowlready.py
@@ -8,23 +8,28 @@ import rospy
 from std_srvs.srv import Empty, EmptyResponse
 from know_cra.srv import OwlreadyLoadOntology, OwlreadyLoadOntologyResponse
 from know_cra.srv import OwlreadyInsertDeleteQuery, OwlreadyInsertDeleteQueryResponse
+from know_cra.srv import OwlreadySelectQuery, OwlreadySelectQueryResponse
+
 
 from know_cra.owlready_wrapper import OWLreadyWrapper
+from know_cra.json_module import JSONmodule
 
 
-class ROSowlreadyExecutor:
+class ROSowlready:
     def __init__(self):
         rospy.init_node("ros_owlready_node", sys.argv)
         rospy.loginfo(rospy.get_name() + ": ROS owlready node has been initialized.")
 
         # services
         self.load_ontology_ = rospy.Service("~load_ontology", OwlreadyLoadOntology, self.load_ontology)
-        self.load_ontology_ = rospy.Service("~syncronize_reasoner", Empty, self.syncronize_reasoner)
-        self.load_ontology_ = rospy.Service("~query_ontology", OwlreadyInsertDeleteQuery, self.query_ontology)
+        self.syncronize_reasoner_ = rospy.Service("~syncronize_reasoner", Empty, self.syncronize_reasoner)
+        self.query_ontology_insert_delete = rospy.Service("~query_ontology_insert_delete", OwlreadyInsertDeleteQuery, self.query_ontology_insert_delete)
+        self.query_ontology_select = rospy.Service("~query_ontology_select", OwlreadySelectQuery, self.query_ontology_select)
 
         # variables
         self.ontology_is_loaded_ = False
         self.orwp_ = OWLreadyWrapper()
+        self.json_mod_ = JSONmodule()
 
 
     def load_ontology(self, req):
@@ -56,7 +61,7 @@ class ROSowlreadyExecutor:
 
         return EmptyResponse()
 
-    def query_ontology(self, req):
+    def query_ontology_insert_delete(self, req):
         rospy.loginfo(rospy.get_name() + ": Received request to update the ontology (using SPARQL).")
 
         if self.ontology_is_loaded_:
@@ -70,8 +75,23 @@ class ROSowlreadyExecutor:
 
         return OwlreadyInsertDeleteQueryResponse()
 
+    def query_ontology_select(self, req):
+        rospy.loginfo(rospy.get_name() + ": Received request to query the ontology (using SPARQL).")
+
+        if self.ontology_is_loaded_:
+            try:
+                rospy.loginfo(rospy.get_name() + ": Querying the ontology.")
+                answer_dict = self.orwp_.sparql_select_query_wp(req.query_str)
+                answer_json_str = self.json_mod_.dictionary_to_json_string(answer_dict)
+            except rospy.ServiceException as e:
+                rospy.logerr(rospy.get_name() + ": Service call failed: %s" % e)
+        else:
+            rospy.loginfo(rospy.get_name() + ": The query has not been performed because none ontology has been loaded.")
+
+        return OwlreadySelectQueryResponse(answer_json_str)
+        
 
 if __name__ == "__main__":
-    ror = ROSowlreadyExecutor()
+    ror = ROSowlready()
     rospy.spin()
 
--- src/know_cra/owlready_wrapper.py
+++ src/know_cra/owlready_wrapper.py
@@ -18,3 +18,32 @@ class OWLreadyWrapper:
     def sparql_insert_delete_query_wp(self, query_string):
         with self.loaded_ontology_:
             default_world.sparql(query_string)
+
+    def sparql_select_query_wp(self, query_string):
+        with self.loaded_ontology_:
+            answer_list = list(default_world.sparql(query_string))
+
+            answer_dict = self.build_dictionary_from_sparql_answer(query_string, answer_list)
+
+        return answer_dict
+
+    def build_dictionary_from_sparql_answer(self, query_string, answer_list):
+        # extract targets in query
+        sub1 = "SELECT " # note the space after 'SELECT' 
+        sub2 = "{"
+
+        idx1 = query_string.index(sub1)
+        idx2 = query_string.index(sub2)
+
+        all_targets_str = query_string[idx1 + len(sub1) + 1 : idx2].strip()
+        all_targets_str = ''.join(all_targets_str.split()) # remove all whitespaces
+        all_targets_list = all_targets_str.split("?")
+
+        # build dictionary
+        answer_dict = dict()
+        for j in range(0, len(all_targets_list)):
+            answer_dict[all_targets_list[j]] = list()
+            for answer in answer_list:
+                answer_dict[all_targets_list[j]].append(answer[j])
+        
+        return answer_dict
